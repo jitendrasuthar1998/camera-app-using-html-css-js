421237d17fe597bdb08d0c32c76e8fcc455785cf
jitendrasuthar <jitendrasuthar995@gmail.com>
2023-09-22 02:41:03 +0530

    working on adding image filter functionality

--- index.js
+++ index.js
@@ -12,34 +12,34 @@ let constraints = {
   audio: true,
 };
 
-navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
-  video.srcObject = stream;
+// navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+//   video.srcObject = stream;
 
-  recorder = new MediaRecorder(stream);
+//   recorder = new MediaRecorder(stream);
 
-  recorder.addEventListener("start", (e) => {
-    chunks = [];
-  });
+//   recorder.addEventListener("start", (e) => {
+//     chunks = [];
+//   });
 
-  recorder.addEventListener("dataavailable", (e) => {
-    chunks.push(e.data);
-  });
+//   recorder.addEventListener("dataavailable", (e) => {
+//     chunks.push(e.data);
+//   });
 
-  recorder.addEventListener("stop", (e) => {
-    // converstion of media chunks data to video
-    let blob = new Blob(chunks, { type: "video/mp4" });
+//   recorder.addEventListener("stop", (e) => {
+//     // converstion of media chunks data to video
+//     let blob = new Blob(chunks, { type: "video/mp4" });
 
-    let videoUrl = window.URL.createObjectURL(blob);
+//     let videoUrl = window.URL.createObjectURL(blob);
 
-    let a = document.createElement("a");
+//     let a = document.createElement("a");
 
-    a.href = videoUrl;
+//     a.href = videoUrl;
 
-    a.download = "stream.mp4";
+//     a.download = "stream.mp4";
 
-    a.click();
-  });
-});
+//     a.click();
+//   });
+// });
 
 recordBtnCont.addEventListener("click", (e) => {
   if (!recorder) return;
